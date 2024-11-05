05d889521aa6b7c6e1f3d29f9c2276577510b62d
Ashwin-3cS <flashashwin2004@gmail.com>
2024-11-05 20:40:12 +0530

    added a script file for simpleStorage.sol

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast(); // start broadcasting means we can send transactions to the network , but the tests will be written above this line
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
